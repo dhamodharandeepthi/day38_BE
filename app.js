61062d647d2d1064a9c6ba77b9c4625df92c1d6d
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2023-12-30 12:42:46 +0530

    start the server

---
+++ app.js
@@ -0,0 +1,8 @@
+// require express
+const express = require('express');
+
+// create a new export app 
+const app = express();
+
+// export the app
+module.exports = app;
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
 const config = require("./utils/config");
+const app = require('./app');
+
 
 console.log('connecting to mongodb...');
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
     console.log("connected to MONGODB...");
+
+    // start the server
+    app.listen(config.PORT, () => {
+      console.log(`Server running on port ${config.PORT}`);
+    })
   })
   .catch((error) => {
       console.log('error connecting to MONGODB:', error.message);
