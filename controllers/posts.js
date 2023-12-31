8799ce027a1154e8c4e160aa2bca07fd5516c125
Dhamodharandeepthi <rdhamodharan22@gmail.com>
2023-12-31 15:46:22 +0530

    viewAllpost added

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,32 +1,43 @@
-const Post = require('../models/post');
+const Post = require("../models/post");
 
 const postContorller = {
-    createPost: (req, res) => {
-        try {
-            // get the userid from the request
-            const userId = req.userId;
+  createPost: (req, res) => {
+    try {
+      // get the userid from the request
+      const userId = req.userId;
 
-            const { title, description } = req.body;
-            
-            // create a POST
-            const post = new Post({
-                title,
-                description,
-                user:userId,
-            })
+      const { title, description } = req.body;
 
-            // save the post
-            post.save();
+      // create a POST
+      const post = new Post({
+        title,
+        description,
+        user: userId,
+      });
 
-            // return the post
+      // save the post
+      post.save();
 
-            return res.json({ message: 'Post created successfully', post });
-        
-        } catch (error) {
-            return res.json({ error: 'Token is invalid' });
-       }
-    },
-}
+      // return the post
 
+      return res.json({ message: "Post created successfully", post });
+    } catch (error) {
+      return res.json({ error: "Token is invalid" });
+    }
+  },
+
+  viewAllPosts: async (req, res) => {
+      try {
+        const userId = req.userId
+      // get all the posts
+      const posts = await Post.find({user: userId});
+
+      //return the posts
+      return res.json({ message: "post retrieved successfully", posts });
+    } catch (error) {
+      return res.json({ error: "token is invalid" });
+    }
+  },
+};
 
 module.exports = postContorller;
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,6 +4,7 @@ const postRouter = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 
 postRouter.post("/", authMiddleware.verifytoken,postContorller.createPost);
+postRouter.get('/', authMiddleware.verifytoken, postContorller.viewAllPosts);
 
 
 module.exports = postRouter;
